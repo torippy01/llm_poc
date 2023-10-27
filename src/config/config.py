8ff48ec749cd38c707e5a8fad69ef234970f9656
Hiroya Miura <torippy.rancha@gmail.com>
2023-10-27 20:07:51 +0900

    interactiveを廃止し、AgentExecutionModeを導入
    * エージェントの実行モードをEnum型で定義

--- src/config/config.py
+++ src/config/config.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import Dict, List
+from enum import Enum, auto
+from typing import Dict, List, Union
 
 from mdutils.mdutils import MdUtils
 import toml
@@ -14,11 +15,55 @@ from tools.uc import create_user_context_predictor_tool
 from utils.utility import sep_md, Self
 
 
+class AgentExecutionMode(Enum):
+    """
+    エージェントの実行方式を定義
+    * QA
+        - 事前に準備した問題集に対してエージェントが回答
+        - 問題集を全て回答し次第、チェイン終了
+
+    * INTERACTIVE
+        - チャット形式で人間
+        - 特定のキーワードを人間が入力し次第、チェイン終了
+
+    * SINGLE
+        - 単一の問に対してエージェントが回答
+        - 回答し次第、チェイン終了
+    """
+
+    QA = auto()
+    INTERACTIVE = auto()
+    SINGLE = auto()
+
+    @classmethod
+    def from_str(cls, string: str) -> Union[Enum, None]:
+        """
+        列挙型の値に相当する文字列がある場合のみ`AgentExecutionMode`の列挙型データを返す．
+        `string`の大文字・小文字は考慮しなくてよい．
+        上記の条件に一致しない場合は全て`None`を返す．
+        """
+        if not string:
+            return None
+
+        string = string.upper()
+
+        if string == cls.QA.name:
+            return cls.QA
+
+        elif string == cls.INTERACTIVE.name:
+            return cls.INTERACTIVE
+
+        elif string == cls.SINGLE.name:
+            return cls.SINGLE
+
+        else:
+            return None
+
 
 @dataclass(frozen=True)
 class Config:
 
-    is_interactive: bool
+    agent_execution_mode: Enum
     agent_type: str
     llm_name: str
     user_index_dir: str
@@ -27,7 +72,6 @@ class Config:
     eval_sentences_path: str
     md_filepath: str
     md_title: str
-
 
     def get_tools(self) -> List[BaseTool]:
         tools : List[BaseTool] = list()
@@ -86,17 +130,31 @@ class Config:
             with open(args.conf_toml, "r") as f:
                 toml_data = toml.load(f)
 
+            agent_execution_mode = AgentExecutionMode.from_str(
+                string=toml_data.get("agent_execution_mode", "")
+            )
+            if agent_execution_mode is None:
+                raise ValueError("agent_execution_modeの値が不正です")
+
+            eval_sentences_path = toml_data.get("eval_sentence", None)
+
+            # QA, Singleの場合、評価文は必須
+            if agent_execution_mode in [AgentExecutionMode.QA, AgentExecutionMode.SINGLE] \
+                and eval_sentences_path is None:
+                raise RuntimeError(
+                    f"agent_execution_modeを{AgentExecutionMode.QA.name}または"
+                    "{AgentExecutionMode.SINGLE.name}とする場合はeval_sentences_path"
+                    "を設定してください"
+                )
+
             conf = Config(
-                is_interactive=toml_data.get("interactive", False),
+                agent_execution_mode=agent_execution_mode,
                 agent_type=toml_data.get("agent_type", "zero-shot-react-description"),
                 llm_name=toml_data.get("llm", "gpt-4"),
                 user_index_dir=toml_data.get("user_index_dir", "user_context_index"),
                 pull=toml_data.get("pull", None),
                 tools_conf=toml_data.get("tools_conf", {}),
-                eval_sentences_path=toml_data.get(
-                    "eval_sentence",
-                    "eval_sentence/test_ai_answer.yaml"
-                ),
+                eval_sentences_path=eval_sentences_path,
                 md_filepath=toml_data.get("md_filepath", "./repo/results/test.md"),
                 md_title=toml_data.get("md_title", "TEST"),
             )
