5604094233768321997aa829558f1770ba548b57
Sohma S <sohma.s.8209@gmail.com>
2023-10-29 17:35:37 +0900

    created callback_handler

--- src/callback/callback.py
+++ src/callback/callback.py
@@ -7,17 +7,19 @@ from mdutils.mdutils import MdUtils
 from tiktoken import encoding_for_model
 
 from conv_log.conv_log import ConversationLog
+from evaluation.evaluation_sentences import EvaluateSentence
+
 
 
 class CustomCallbackHandler(BaseCallbackHandler):
-    md: MdUtils
-    conversation_logs: List[ConversationLog] = list()
-    user_input: str
-    chain_start_time: float
 
-    def __init__(self, md: MdUtils):
+    def __init__(self, md_file: MdUtils):
         super().__init__()
-        self.md = md
+        self.user_input: Optional[str] = None
+        self.chain_start_time:float = 0
+        self.md_file: MdUtils = md_file
+        self.e_sentence_list: List[EvaluateSentence] = list()
+
 
     def on_chain_start(
         self,
@@ -30,22 +32,28 @@ class CustomCallbackHandler(BaseCallbackHandler):
         metadata: Optional[Dict[str, Any]] = None,
         **kwargs: Any,
     ) -> None:
-        """Run when chain starts running."""
+
         user_input = inputs.get("input")
         if user_input is None:
-            raise RuntimeError("ユーザーは必ず入力文を入れているはずなのにありません！これはいくないです！")
+            raise TypeError("Invalid type: user_input is NoneType")
 
         self.user_input = user_input
         self.chain_start_time = time()
         return
 
-    def on_text(self, text: str, **kwargs: Any) -> None:
-        """Run on arbitrary text."""
+
+    def on_text(
+        self,
+        text: str,
+        **kwargs: Any
+    ) -> None:
+
         encoding = encoding_for_model("gpt-4")
         token_count = len(encoding.encode(text))
         if token_count > 5000:
             print(f"token count is {token_count}")
         return
+
 
     def on_chain_end(
         self,
@@ -55,11 +63,13 @@ class CustomCallbackHandler(BaseCallbackHandler):
         parent_run_id: Optional[UUID] = None,
         **kwargs: Any,
     ) -> None:
+
         output = outputs.get("output", None)
         if output is None:
-            raise RuntimeError("チェインが終わったというのに最終回答がありません！これはいくないです！")
+            raise TypeError("Invalid type: output is NoneType")
 
         elapsed_time = time() - self.chain_start_time
+
         conversation_log = ConversationLog(
             input=self.user_input,
             output=output,
@@ -67,5 +77,14 @@ class CustomCallbackHandler(BaseCallbackHandler):
             chat_history=outputs.get("chat_history", None),
             elapsed_time=elapsed_time,
         )
-        conversation_log.dump(self.md)
+        conversation_log.dump(self.md_file)
+
+        self.e_sentence_list.append(
+            EvaluateSentence(
+                input=self.user_input,
+                output=output,
+                human_answer=None,
+                evaluation=None,
+            )
+        )
         return
