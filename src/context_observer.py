fa6072f7121c53217951905de56bca13a332dde0
Hiroya Miura <torippy.rancha@gmail.com>
2023-11-07 10:29:36 +0900

    host, portの内容を検証

--- src/context_observer.py
+++ src/context_observer.py
@@ -5,6 +5,7 @@ python3 src/validate_api.py -tm conf/beta.toml
 """
 
 import os
+import re
 import uvicorn
 
 from fastapi import FastAPI
@@ -12,7 +13,7 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from context_fetcher.github_wiki import GithubWiki
-
+from typing import Optional
 
 load_dotenv()
 app = FastAPI()
@@ -28,8 +29,7 @@ class Request(BaseModel):
 async def github_wiki(request: Request):
     if request.action not in ["created", "edited", "deleted"]:
         raise HTTPException(
-            status_code=500,
-            detail="Unexpected error is occured on github page update."
+            status_code=500, detail="Unexpected error is occured on github page update."
         )
     wiki = GithubWiki(url=request.url)
     wiki.create_index()
@@ -42,7 +42,29 @@ async def health():
     return
 
 
+def host_validation(host: Optional[str]):
+    # hostが文字列であればTrue
+    # TODO: 文字列の内容を加味すべき
+    if not host:
+        return False
+    elif isinstance(host, str):
+        return True
+
+
+def port_validation(port: Optional[str]):
+    # portが半角数字文字列であればTrue
+    # それ以外はFalse
+    if not port:
+        return False
+    return True if re.fullmatch("[0-9]+", port) else False
+
+
 if __name__ == "__main__":
-    host = os.environ.get("CONTENT_OBSERVER_SV_HOST")
-    port = int(os.environ.get("CONTENT_OBSERVER_SV_PORT"))
-    uvicorn.run("content_observer:app", port=port, reload=True, host=host)
+    # CONTENT_OBSERVER_SV_HOSTから値が取れない場合は`localhost`
+    _host = os.environ.get("CONTENT_OBSERVER_SV_HOST")
+    host = _host if host_validation(_host) else "localhost"
+
+    # CONTENT_OBSERVER_SV_PORTから値が取れない場合は`3010`
+    _port = os.environ.get("CONTENT_OBSERVER_SV_PORT")
+    port = int(_port) if port_validation(_port) else 3010  # type: ignore
+    uvicorn.run("content_observer:app", port=port, reload=True, host=host)  # type: ignore
