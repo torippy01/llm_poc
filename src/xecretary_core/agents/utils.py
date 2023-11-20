f4d1412a9e5619175416e7e4700c30f3da34745a
Hiroya Miura <torippy.rancha@gmail.com>
2023-11-20 12:41:06 +0900

    hubを当面使わないので削除

--- src/xecretary_core/agents/utils.py
+++ src/xecretary_core/agents/utils.py
@@ -1,36 +1,4 @@
-from typing import Any
-
-from langchain import hub
 from langchain.agents.agent_types import AgentType
-from langchain.agents.format_scratchpad import format_log_to_str
-from langchain.agents.output_parsers import ReActSingleInputOutputParser
-from langchain.chat_models.base import BaseChatModel
-
-
-# hub.pull()の結果がpromptを前提としているが、実際はその限りでない
-# TODO: hub.pullの結果を判別してagentに適用するよう修正
-
-
-def fetch_agent_from_hub(
-    pull, llm: BaseChatModel, tool_description: str, tool_names: str
-) -> Any:
-    ## need to modify
-    prompt = hub.pull(pull)
-    prompt = prompt.partial(
-        tools=tool_description,
-        tool_names=tool_names,
-    )
-
-    return (
-        {
-            "input": lambda x: x["input"],
-            "agent_scratchpad": lambda x: format_log_to_str(x["intermediate_steps"]),
-            "chat_history": lambda x: x["chat_history"],
-        }
-        | prompt
-        | llm.bind(stop=["\nObservation"])
-        | ReActSingleInputOutputParser()
-    )
 
 
 def agent_types_from_string(agent_type_str: str) -> AgentType:
@@ -64,4 +32,4 @@ def agent_types_from_string(agent_type_str: str) -> AgentType:
         return AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION
 
     else:
-        raise ValueError("Invalid value : agent_type_str")
+        raise ValueError("Invalid value : 'agent_type_str'")
